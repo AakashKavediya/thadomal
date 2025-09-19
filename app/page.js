4ddfbddaf85c5686a355aac98ab2892b7c5424fb
Aakash Kavediya <kavediyaaakash@gmail.com>
2025-09-19 09:42:17 +0530

    Addition of hero section

--- app/page.js
+++ app/page.js
@@ -3,13 +3,13 @@ import React, { useState, useEffect } from "react";
 import App from "./CustomComponents/searchFood";
 import RecipeSuggestion from "./CustomComponents/resipieSuggestion";
 import Header from "./CustomComponents/header";
+import HeroSection from "./CustomComponents/heroSection";
 
 export default function HomePage() {
   return(
     <div>
         <div>
-          <Header />
-          <RecipeSuggestion />
+          <HeroSection />
         </div>
     </div>
   )
